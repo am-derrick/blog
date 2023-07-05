77ccfc065b921c85e5b721941920edd5f190ebdb
Derrick Ampire <ampire90@gmail.com>
2023-07-05 11:41:47 +0300

    app context added

--- blog.py
+++ blog.py
@@ -1,14 +1,42 @@
 import os
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import SignUpForm, LogInForm
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = str(os.environ.get('SECRET_KEY'))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-
 db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    img_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User '{self.username}' with email '{self.email}'"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    post_date = db.Column(db.DateTime, nullable=False,
+                          default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post 'Title: {self.title}' Created at: '{self.content}'"
+
+
+# app context
+with app.app_context():
+    db.create_all()
 
 posts = [
     {
