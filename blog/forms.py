513a6804b7c604c794a09424b658fddf5e7c0c48
Derrick Ampire <ampire90@gmail.com>
2023-07-06 10:48:29 +0300

    validation field functions added

--- blog/forms.py
+++ blog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import Length, DataRequired, Email, EqualTo
+from wtforms.validators import Length, DataRequired, Email, EqualTo, ValidationError
+from blog.models import User
 
 
 class SignUpForm(FlaskForm):
@@ -11,6 +12,17 @@ class SignUpForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password',
                                      validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Username exits, choose a different one.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError(
+                'Email exits, choose a different one or log in if you have an account.')
 
 
 class LogInForm(FlaskForm):
