edd9475fa016dede318355e20bb4d8f3f4c770c1
Derrick Ampire <ampire90@gmail.com>
2023-07-06 13:25:02 +0300

    redirect function edited if user logged in

--- blog/routes.py
+++ blog/routes.py
@@ -1,5 +1,5 @@
 from blog.models import User, Post
-from flask import render_template, url_for, flash, redirect
+from flask import render_template, url_for, flash, request, redirect
 from blog.forms import SignUpForm, LogInForm
 from blog import app, db, bcrypt
 from flask_login import login_user, current_user, logout_user, login_required
@@ -62,7 +62,8 @@ def login():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
-            return redirect(url_for('home'))
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
             flash('Error! Please check your email and password.', 'danger')
     return render_template('login.html', title='Log In', form=form)
