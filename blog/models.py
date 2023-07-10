bbca87ea93dd5267499816b724f9c72d017234e9
Derrick Ampire <ampire90@gmail.com>
2023-07-10 21:44:29 +0300

    default img extension changed

--- blog/models.py
+++ blog/models.py
@@ -13,7 +13,7 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
-    img_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    img_file = db.Column(db.String(20), nullable=False, default='default.png')
     posts = db.relationship('Post', backref='author', lazy=True)
 
     def __repr__(self):
